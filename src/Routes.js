4ce208a9d17e98f8f4a531fde057652fcb3d3292
jamezjaz <jamezjaz@gmail.com>
2021-03-01 12:28:35 -0600

    docs: Implements food details page (show page) with hard coded data

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import FoodDetails from './components/FoodDetails';
 import Nav from './components/Nav';
 import FoodsList from './container/FoodsList';
 
@@ -8,6 +9,7 @@ const Routes = () => (
     <Nav />
     <Switch>
       <Route exact path="/" component={FoodsList} />
+      <Route path="/foodDetails" component={FoodDetails} />
     </Switch>
   </Router>
 );
