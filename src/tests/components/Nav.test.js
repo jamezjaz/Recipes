3419b95f6e0de7e5c5b8d1d454c118dbc3ca3072
jamezjaz <jamezjaz@gmail.com>
2021-03-03 05:44:29 -0600

    test: Add more tests to Nav component

--- src/tests/components/Nav.test.js
+++ src/tests/components/Nav.test.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { cleanup } from '@testing-library/react';
+import { cleanup, screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import renderer from 'react-test-renderer';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Nav from '../../components/Nav';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Nav />', () => {
   afterEach(cleanup);
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Nav />);
+  });
 
   test('renders correctly', () => {
     const tree = renderer
@@ -16,6 +26,30 @@ describe('<Nav />', () => {
       )
       .toJSON();
       expect(tree).toMatchSnapshot();
+  });
+
+  test('contains Catalogue Of Recipes header', () => {
+    render(
+      <Router>
+        <Nav />
+      </Router>,
+    );
+    expect(screen.getByText('Catalogue Of Recipes')).toBeInTheDocument();
+  });
+
+  test('it contains li element', () => {
+    const li = <li>Recipes</li>;
+    expect(wrapper.containsMatchingElement(li)).toBe(true);
+  });
+
+  test('h2 element has a text', () => {
+    const h2 = wrapper.find('.my-logo');
+    expect(h2.text()).toEqual('Catalogue Of Recipes');
+  });
+
+  test('it contains a Link', () => {
+    const link = <Link><li>About Us</li></Link>;
+    expect(wrapper.containsMatchingElement(link)).toBe(true);
   });
 });
 
