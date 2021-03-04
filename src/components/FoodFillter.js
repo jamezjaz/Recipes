9bdf805871f2232b9a3a7391944b4bd10ba01b0b
jamezjaz <jamezjaz@gmail.com>
2021-03-04 12:42:06 -0600

    docs: Modifies the select box

--- src/components/FoodFillter.js
+++ src/components/FoodFillter.js
@@ -28,9 +28,15 @@ const FoodFilter = props => {
     <div>
       <label htmlFor="select" className="mx-auto my-0">
         <select className={FoodFilterStyles.select} name="category" id="select" data-testid="categories" onChange={handleClick}>
-          <option disable="true" hidden>FOOD CATEGORIES</option>
+          <option disable="true" hidden>
+            FOOD CATEGORIES
+          </option>
           {categories.map(
-            category => <option value={category} key={category}>{category}</option>,
+            category => (
+              <option value={category} key={category}>
+                {category}
+              </option>
+            ),
           )}
         </select>
       </label>
